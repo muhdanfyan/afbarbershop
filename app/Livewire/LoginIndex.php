0043ad200ce1686822499875bd396566bcad9dc9
hayatulhabirun88 <hayatulhabirun@gmail.com>
2025-08-26 23:25:28 +0800

    menambahkan front login

---
+++ app/Livewire/LoginIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LoginIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.login-index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Backend\LoginController;
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
